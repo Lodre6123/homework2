cbf4225cf92fae1f0bd27ec19c2053f322946a70
Lodre6123 <nikpash6123@mail.ru>
2023-02-04 20:41:15 +0300

    izi

---
+++ zad2/Program.cs
@@ -0,0 +1,16 @@
+﻿/*Напишите программу, которая выводит третью цифру заданного числа.
+645 -> 5
+32679 -> 6
+21 -> -1
+*/
+
+Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+while (num > 1000) num = num / 10;
+if (num < 100) Console.WriteLine("У данного числа, нет третьей цифры");
+else
+{
+    int Number = num % 10;
+    Console.WriteLine($"Третье цифра {num} - {Number}");
+}
